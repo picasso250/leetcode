e2680c1b587521792c3e84d910e9643c02161581
xiaochi <wxiaochi@qq.com>
2021-04-12 16:06:28 +0800

    x

--- go/linklist.go
+++ go/linklist.go
@@ -1,1 +1,110 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//  * Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	p := head
+	var prevDel *ListNode = nil
+	var toDel *ListNode = nil
+	var prev *ListNode
+	i := 0
+	for p != nil {
+		if i >= n-1 {
+			prevDel = prev
+		}
+		if i >= n {
+			toDel = p
+		}
+		i++
+		prev = p
+		p = p.Next
+	}
+	// del
+	head = head.delNode(toDel, prevDel)
+	return head
+}
+func (head *ListNode) delNode(toDel *ListNode, prevDel *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	if toDel == head {
+		newHead := head.Next
+		head.Next = nil
+		return newHead
+	}
+	if prevDel != nil {
+		if prevDel.Next != toDel {
+			panic("prevDel's next is not toDel")
+		}
+		prevDel.Next = toDel.Next
+		toDel.Next = nil
+	}
+	return head
+}
+func toList(lst []int) *ListNode {
+	if len(lst) == 0 {
+		return nil
+	}
+	l := make([]ListNode, len(lst))
+	for i, v := range lst {
+		var next *ListNode
+		if i == len(lst)-1 {
+			next = nil
+		} else {
+			next = &l[i+1]
+		}
+		l[i] = ListNode{v, next}
+	}
+	return &l[0]
+}
+func (head *ListNode) toSlice() []int {
+	a := make([]int, 0)
+	for p := head; p != nil; p = p.Next {
+		a = append(a, p.Val)
+	}
+	return a
+}
+func (head *ListNode) toStrings() []string {
+	a := make([]string, 0)
+	for p := head; p != nil; p = p.Next {
+		a = append(a, strconv.Itoa(p.Val))
+	}
+	return a
+}
+func (head *ListNode) ToString() string {
+	return fmt.Sprintf("[%s]", strings.Join(head.toStrings(), ","))
+}
+func deleteDuplicates(head *ListNode) *ListNode {
+	dummy := &ListNode{0, head}
+	for p := dummy; p != nil; {
+		if p.Next != nil && p.Next.Next != nil && p.Next.Val == p.Next.Next.Val {
+			p = deleteDuplicatesN(p, p.Next.Val)
+		} else {
+			p = p.Next
+		}
+	}
+	return dummy.Next
+}
+func deleteDuplicatesN(dummy *ListNode, val int) *ListNode {
+	for p := dummy; p.Next != nil; {
+		if p.Next.Val == val {
+			p.Next = p.Next.Next
+		} else {
+			break
+		}
+	}
+	return dummy
+}
--- go/tree.go
+++ go/tree.go
@@ -1,6 +1,8 @@
 package main
 
-//   Definition for a binary tree node.
+import "fmt"
+
+//    Definition for a binary tree node.
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -27,12 +29,140 @@ func isValidBSTIter(root *TreeNode) (isValid bool, max, min int) {
 	min = minInts(leftMin, rightMin, root.Val)
 	return
 }
-func treeToList(root *TreeNode) (ret []int) {
+
+// func treeToList(root *TreeNode) (ret []int) {
+// 	if root == nil {
+// 		return
+// 	}
+// 	ret = []int{root.Val}
+// 	ret = append(ret, treeToList(root.Left)...)
+// 	ret = append(ret, treeToList(root.Right)...)
+// 	return
+// }
+func recoverTree(root *TreeNode) {
 	if root == nil {
 		return
 	}
-	ret = []int{root.Val}
-	ret = append(ret, treeToList(root.Left)...)
-	ret = append(ret, treeToList(root.Right)...)
+	lst := midOrderPointer(root)
+	i, j := -1, -1
+	for k := 0; k < len(lst)-1; k++ {
+		if lst[k].Val > lst[k+1].Val {
+			if i == -1 {
+				i = k
+			} else {
+				j = k
+			}
+		}
+	}
+
+	// mapParent := make(map[*TreeNode]*TreeNode)
+	// mapDirection := make(map[*TreeNode]bool)
+	// treeInfo(root, mapParent, mapDirection)
+	a, b := lst[i], lst[j+1]
+	if j == -1 {
+		a, b = lst[i], lst[i+1]
+	}
+	a.Val, b.Val = b.Val, a.Val
+}
+
+func midOrderPointer(root *TreeNode) (ret []*TreeNode) {
+	if root == nil {
+		return
+	}
+	ret = append(ret, midOrderPointer(root.Left)...)
+	ret = append(ret, root)
+	ret = append(ret, midOrderPointer(root.Right)...)
 	return
+}
+
+const TreeLeft = true
+const TreeRight = false
+
+func treeInfo(root *TreeNode,
+	mapParent map[*TreeNode]*TreeNode,
+	mapDirection map[*TreeNode]bool) {
+	if root == nil {
+		return
+	}
+	mapParent[root.Left] = root
+	mapParent[root.Right] = root
+	mapDirection[root.Left] = TreeLeft
+	mapDirection[root.Right] = TreeRight
+	treeInfo(root.Left, mapParent, mapDirection)
+	treeInfo(root.Right, mapParent, mapDirection)
+}
+func buildTree(a []interface{}) *TreeNode {
+	n := len(a)
+	if n == 0 {
+		return nil
+	}
+	ret := make([]*TreeNode, n)
+	for i, v := range a {
+		if v == nil {
+			ret[i] = nil
+		} else {
+			ret[i] = &TreeNode{v.(int), nil, nil}
+		}
+	}
+	for i, v := range ret {
+		if v == nil {
+
+		} else {
+			left := i*2 + 1
+			right := i*2 + 2
+			if left < n {
+				v.Left = ret[left]
+			}
+			if right < n {
+				v.Right = ret[right]
+			}
+		}
+	}
+	return ret[0]
+}
+func treeToList(tree *TreeNode) []interface{} {
+	if tree == nil {
+		return []interface{}{}
+	}
+	i, j := 0, 1
+	ret := []*TreeNode{tree}
+	for {
+		notAllNil := false
+		for k := i; k < j; k++ {
+			if ret[k] != nil {
+				// ret[2*k+1] = ret[k].Left
+				// ret[2*k+2] = ret[k].Right
+				ret = append(ret, ret[k].Left, ret[k].Right)
+				if ret[k].Left != nil || ret[k].Right != nil {
+					notAllNil = true
+				}
+			}
+		}
+		i = 2*i + 1
+		j = 2*(i+1) + 1
+		if !notAllNil {
+			break
+		}
+	}
+	j = len(ret) - 1
+	for j >= 0 && ret[j] == nil {
+		j--
+	}
+	j++
+	r := make([]interface{}, j)
+	for i = 0; i < j; i++ {
+		if ret[i] == nil {
+			r[i] = nil
+		} else {
+			r[i] = ret[i].Val
+		}
+	}
+	return r
+}
+func treeToString(tree *TreeNode) string {
+	if tree == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("(%s %d %s)",
+		treeToString(tree.Left), tree.Val, treeToString(tree.Right))
 }
