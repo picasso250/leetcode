ae9f5a2e0083cc1071e21beb77e02eb45550913f
xiaochi <wxiaochi@qq.com>
2021-04-12 16:39:41 +0800

    x

--- go/tree.go
+++ go/tree.go
@@ -189,3 +189,18 @@ func isBalancedIter(root *TreeNode) (isBal bool, height int) {
 	height = maxInts(leftHeight, rightHeight) + 1
 	return
 }
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	return minInts(minDepth(root.Left), minDepth(root.Right)) + 1
+}
